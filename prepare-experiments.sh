f0053a0e2dc2712a52da00d35b259b3d31ce3950
許筑鈞 <ppp432986@gmail.com>
2023-11-18 07:58:08 +0100

    Update prepare_experiments.sh, launch two kinds of vm (KVM and QEMU)

--- prepare-experiments.sh
+++ prepare-experiments.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# ----
+# Preparation
+
 # Install required system packages:
+sudo apt-get update
 sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients qemu-utils genisoimage virtinst
 
 # Download Ubuntu cloud image:
-wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -O ~/cloud-image/ubuntu-22.04-server.img
+wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -O ~/ubuntu-22.04-server.img
 
 # Create directory for base images:
 sudo mkdir /var/lib/libvirt/images/base
@@ -12,33 +16,32 @@ sudo mkdir /var/lib/libvirt/images/base
 # Move downloaded image into this folder:
 sudo mv ubuntu-22.04-server.img /var/lib/libvirt/images/base/ubuntu-22.04-server.qcow2
 
-#-----
+# -----
 # Virtual machine image
 
 # Create directory for our instance images:
-sudo mkdir /var/lib/libvirt/images/ccex1vm1
+sudo mkdir /var/lib/libvirt/images/kvm-vm
+sudo mkdir /var/lib/libvirt/images/qemu-vm
 
 # Create a disk image based on the Ubuntu image:
-sudo qemu-img create -f qcow2 -F qcow2 -o backing_file=/var/lib/libvirt/images/base/ubuntu-22.04-server.qcow2 /var/lib/libvirt/images/ccex1vm1/ccex1vm1.qcow2
+sudo qemu-img create -f qcow2 -F qcow2 -o backing_file=/var/lib/libvirt/images/base/ubuntu-22.04-server.qcow2 /var/lib/libvirt/images/kvm-vm/kvm-vm.qcow2
+sudo qemu-img create -f qcow2 -F qcow2 -o backing_file=/var/lib/libvirt/images/base/ubuntu-22.04-server.qcow2 /var/lib/libvirt/images/qemu-vm/qemu-vm.qcow2
 
 # Verify that image:
-sudo qemu-img info /var/lib/libvirt/images/ccex1vm1/ccex1vm1.qcow2
-
-# Current virtual size is 2.2 GB, let’s set it to 4 GB:
-sudo qemu-img resize /var/lib/libvirt/images/ccex1vm1/ccex1vm1.qcow2 4G
-
-# Check if resized
-sudo qemu-img info /var/lib/libvirt/images/ccex1vm1/ccex1vm1.qcow2
+sudo qemu-img info /var/lib/libvirt/images/kvm-vm/kvm-vm.qcow2
+sudo qemu-img info /var/lib/libvirt/images/qemu-vm/qemu-vm.qcow2
 
 # -----
 # Cloud-Init Configuration
 
 # Create meta-data:
 cat >meta-data <<EOF
-local-hostname: ccex1vm1
+local-hostname: ccex1vm2
 EOF
 
 # Read public key into environment variable:
+user=$(whoami) && host=$(hostname)
+ssh-keygen -t rsa -C "${user}@${host}" -f ~/.ssh/id_rsa -N ""
 export PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
 
 # Create user-data:
@@ -51,27 +54,30 @@ users:
     sudo: ['ALL=(ALL) NOPASSWD:ALL']
     groups: sudo
     shell: /bin/bash
-runcmd:
-  - echo "AllowUsers ubuntu" >> /etc/ssh/sshd_config
-  - restart ssh
 EOF
 
 # Create a disk to attach with Cloud-Init configuration:
-sudo genisoimage  -output /var/lib/libvirt/images/ccex1vm1/ccex1vm1-cidata.iso -volid cidata -joliet -rock user-data meta-data
+sudo genisoimage  -output /var/lib/libvirt/images/kvm-vm/kvm-vm-cidata.iso -volid cidata -joliet -rock user-data meta-data
+sudo genisoimage  -output /var/lib/libvirt/images/qemu-vm/qemu-vm-cidata.iso -volid cidata -joliet -rock user-data meta-data
 
 # ----
 # Launch virtual machine
 
-sudo virt-install --connect qemu:///system --virt-type kvm --name ubuntu --ram 1024 --vcpus=1 --os-type linux --os-variant ubuntu22.04 --disk path=/var/lib/libvirt/images/ccex1vm1/ccex1vm1.qcow2,format=qcow2 --disk /var/lib/libvirt/images/ccex1vm1/ccex1vm1-cidata.iso,device=cdrom --import --network network=default --noautoconsole
+#KVM
+sudo virt-install --connect qemu:///system --virt-type kvm --name kvm-vm --ram 1024 --vcpus=4 --os-type linux --os-variant ubuntu22.04 --disk path=/var/lib/libvirt/images/kvm-vm/kvm-vm.qcow2,format=qcow2 --disk /var/lib/libvirt/images/kvm-vm/kvm-vm-cidata.iso,device=cdrom --import --network network=default --noautoconsole
+
+#QEMU
+sudo virt-install --connect qemu:///system --virt-type qemu --name qemu-vm --ram 1024 --vcpus=4 --os-type linux --os-variant ubuntu22.04 --disk path=/var/lib/libvirt/images/qemu-vm/qemu-vm.qcow2,format=qcow2 --disk /var/lib/libvirt/images/qemu-vm/qemu-vm-cidata.iso,device=cdrom --import --network network=default --noautoconsole
 
 # Make sure the virtual machine is running:
 sudo virsh list
 
 # Get the IP address:
-sudo virsh domifaddr ubuntu
+sudo virsh domifaddr kvm-vm
+sudo virsh domifaddr qemu-vm
 
 # Connect to the instance by the public key:
-ssh ubuntu@192.168.122.201
+# ssh ubuntu@192.168.122.201
 
 # ----
 # Install Docker on a Google Cloud virtual machine
@@ -81,9 +87,8 @@ sudo apt-get install docker.io
 
 # Verify Docker installation:
 docker --version
-docker run hello-world
 
-----
+# ----
 # Write a Dockerfile with Ubuntu 22.04 and benchmark
 cat <<EOF > Dockerfile
 FROM ubuntu:22.04
@@ -94,16 +99,18 @@ EOF
 
 ----
 # Build the Docker image
-sudo docker build -t benchmark-image .
+sudo docker build -t docker-image .
 
 # Run Docker container in the background
-docker run -d --name benchmark-container benchmark-image
+sudo docker run -d --name docker-container docker-image
 
 # Copy the benchmark script to each VM
-sudo scp benchmark.sh user@<VM_IP>:/home/user/benchmark.sh
+sudo scp -i ~/.ssh/id_rsa benchmark.sh kvm-vm@<VM_IP>:/home/kvm-vm/benchmark.sh
+sudo scp -i ~/.ssh/id_rsa benchmark.sh qemu-vm@<VM_IP>:/home/qemu-vm/benchmark.sh
 
 # Start the VMs
-sudo virsh start ubuntu
+sudo virsh start kvm-vm
+sudo virsh start qemu-vm
 
 # Output a message indicating the setup is complete
-echo "GCP VM setup complete. You can now SSH into the VMs and run experiments."
+echo "GCP VM setup complete."
